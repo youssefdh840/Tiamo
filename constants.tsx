de9d896dcc1518dd123306ce9c216bb80a4424bb
youssefdh840 <tkyoyoussef@gmail.com>
2026-01-10 17:17:30 +0100

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,34 @@
+
+import { Character } from './types';
+
+/**
+ * NOTA PARA EL USUARIO:
+ * Para que el juego funcione offline con tus fotos, coloca tus 23 imágenes
+ * en una carpeta llamada 'images' con los nombres:
+ * 1.png, 2.png, 3.png, ..., 23.png
+ */
+export const CHARACTERS: Character[] = [
+  { id: 1, image: 'images/1.png', traits: { estatura: 'de estatura media', pelo: 'corto', ojos: 'marrones', personalidad: 'simpático' } },
+  { id: 2, image: 'images/2.png', traits: { estatura: 'alto', pelo: 'corto', ojos: 'marrones', personalidad: 'inteligente' } },
+  { id: 3, image: 'images/3.png', traits: { estatura: 'alto', pelo: 'largo', ojos: 'verdes', personalidad: 'amable' } },
+  { id: 4, image: 'images/4.png', traits: { estatura: 'bajo', pelo: 'corto', ojos: 'azules', personalidad: 'serio' } },
+  { id: 5, image: 'images/5.png', traits: { estatura: 'alto', pelo: 'negro', ojos: 'marrones', personalidad: 'serio' } },
+  { id: 6, image: 'images/6.png', traits: { estatura: 'de estatura media', pelo: 'largo', ojos: 'marrones', personalidad: 'amable' } },
+  { id: 7, image: 'images/7.png', traits: { estatura: 'bajo', pelo: 'rubio', ojos: 'azules', personalidad: 'inteligente' } },
+  { id: 8, image: 'images/8.png', traits: { estatura: 'alto', pelo: 'largo', ojos: 'marrones', personalidad: 'simpático' } },
+  { id: 9, image: 'images/9.png', traits: { estatura: 'de estatura media', pelo: 'largo', ojos: 'marrones', personalidad: 'serio' } },
+  { id: 10, image: 'images/10.png', traits: { estatura: 'de estatura media', pelo: 'rubio', ojos: 'azules', personalidad: 'amable' } },
+  { id: 11, image: 'images/11.png', traits: { estatura: 'alto', pelo: 'negro', ojos: 'marrones', personalidad: 'simpático' } },
+  { id: 12, image: 'images/12.png', traits: { estatura: 'de estatura media', pelo: 'corto', ojos: 'marrones', personalidad: 'inteligente' } },
+  { id: 13, image: 'images/13.png', traits: { estatura: 'alto', pelo: 'largo', ojos: 'marrones', personalidad: 'amable' } },
+  { id: 14, image: 'images/14.png', traits: { estatura: 'bajo', pelo: 'corto', ojos: 'marrones', personalidad: 'serio' } },
+  { id: 15, image: 'images/15.png', traits: { estatura: 'bajo', pelo: 'negro', ojos: 'azules', personalidad: 'inteligente' } },
+  { id: 16, image: 'images/16.png', traits: { estatura: 'de estatura media', pelo: 'corto', ojos: 'marrones', personalidad: 'simpático' } },
+  { id: 17, image: 'images/17.png', traits: { estatura: 'de estatura media', pelo: 'largo', ojos: 'marrones', personalidad: 'amable' } },
+  { id: 18, image: 'images/18.png', traits: { estatura: 'bajo', pelo: 'largo', ojos: 'marrones', personalidad: 'simpático' } },
+  { id: 19, image: 'images/19.png', traits: { estatura: 'alto', pelo: 'corto', ojos: 'marrones', personalidad: 'serio' } },
+  { id: 20, image: 'images/20.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'serio' } },
+  { id: 21, image: 'images/21.png', traits: { estatura: 'bajo', pelo: 'corto', ojos: 'marrones', personalidad: 'inteligente' } },
+  { id: 22, image: 'images/22.png', traits: { estatura: 'alto', pelo: 'rubio', ojos: 'azules', personalidad: 'amable' } },
+  { id: 23, image: 'images/23.png', traits: { estatura: 'bajo', pelo: 'negro', ojos: 'marrones', personalidad: 'serio' } },
+];
