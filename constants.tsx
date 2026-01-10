e9b0a58f7d694e596112be1c9558a909261a785c
youssefdh840 <tkyoyoussef@gmail.com>
2026-01-10 23:09:49 +0100

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,28 @@
+
+import { Character } from './types';
+
+export const CHARACTERS: Character[] = [
+  { id: 1, name: 'Hanno Tauber', image: 'images/1.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'serio', caracteristica: 'barba' } },
+  { id: 2, name: 'Phoebe', image: 'images/2.png', traits: { estatura: 'de estatura media', pelo: 'rubio', ojos: 'marrones', personalidad: 'simpático', caracteristica: 'mujer rubia' } },
+  { id: 3, name: 'Rachel', image: 'images/3.png', traits: { estatura: 'de estatura media', pelo: 'castaño', ojos: 'marrones', personalidad: 'amable', caracteristica: 'mujer morena' } },
+  { id: 4, name: 'Chica Pecosa', image: 'images/4.png', traits: { estatura: 'bajo', pelo: 'castaño', ojos: 'marrones', personalidad: 'amable', caracteristica: 'mujer morena' } },
+  { id: 5, name: 'Chica Morena', image: 'images/5.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'simpático', caracteristica: 'mujer morena' } },
+  { id: 6, name: 'Chuck Bass', image: 'images/6.png', traits: { estatura: 'alto', pelo: 'negro', ojos: 'marrones', personalidad: 'serio', caracteristica: 'barba' } },
+  { id: 7, name: 'Jack Rooney', image: 'images/7.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'serio', caracteristica: 'barba' } },
+  { id: 8, name: 'Chica Floral', image: 'images/8.png', traits: { estatura: 'de estatura media', pelo: 'castaño', ojos: 'marrones', personalidad: 'amable', caracteristica: 'mujer morena' } },
+  { id: 9, name: 'Chishiya', image: 'images/9.png', traits: { estatura: 'de estatura media', pelo: 'blanco', ojos: 'marrones', personalidad: 'inteligente', caracteristica: 'cara alargada' } },
+  { id: 10, name: 'Marilyn', image: 'images/10.png', traits: { estatura: 'bajo', pelo: 'rubio', ojos: 'marrones', personalidad: 'simpático', caracteristica: 'mujer rubia' } },
+  { id: 11, name: 'Jonas Adulto', image: 'images/11.png', traits: { estatura: 'alto', pelo: 'castaño', ojos: 'marrones', personalidad: 'serio', caracteristica: 'barba' } },
+  { id: 12, name: 'Jonas Joven', image: 'images/12.png', traits: { estatura: 'de estatura media', pelo: 'castaño', ojos: 'azules', personalidad: 'serio', caracteristica: 'cara cuadrada' } },
+  { id: 13, name: 'Killua', image: 'images/13.png', traits: { estatura: 'bajo', pelo: 'blanco', ojos: 'azules', personalidad: 'inteligente', caracteristica: 'cara alargada' } },
+  { id: 14, name: 'Max', image: 'images/14.png', traits: { estatura: 'bajo', pelo: 'pelirrojo', ojos: 'azules', personalidad: 'serio', caracteristica: 'mujer morena' } },
+  { id: 15, name: 'Mun-yeong', image: 'images/15.png', traits: { estatura: 'alto', pelo: 'negro', ojos: 'marrones', personalidad: 'serio', caracteristica: 'cara cuadrada' } },
+  { id: 16, name: 'Auggie', image: 'images/16.png', traits: { estatura: 'alto', pelo: 'castaño', ojos: 'marrones', personalidad: 'inteligente', caracteristica: 'mujer morena' } },
+  { id: 17, name: 'Eleven', image: 'images/17.png', traits: { estatura: 'bajo', pelo: 'castaño', ojos: 'marrones', personalidad: 'serio', caracteristica: 'mujer morena' } },
+  { id: 18, name: 'Joey', image: 'images/18.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'simpático', caracteristica: 'cara cuadrada' } },
+  { id: 19, name: 'Leonor', image: 'images/19.png', traits: { estatura: 'de estatura media', pelo: 'rubio', ojos: 'azules', personalidad: 'amable', caracteristica: 'mujer rubia' } },
+  { id: 20, name: 'Batistuta', image: 'images/20.png', traits: { estatura: 'alto', pelo: 'castaño', ojos: 'marrones', personalidad: 'serio', caracteristica: 'barba' } },
+  { id: 21, name: 'Mujer Pelirroja', image: 'images/21.png', traits: { estatura: 'de estatura media', pelo: 'pelirrojo', ojos: 'verdes', personalidad: 'amable', caracteristica: 'mujer morena' } },
+  { id: 22, name: 'Satoru', image: 'images/22.png', traits: { estatura: 'alto', pelo: 'blanco', ojos: 'azules', personalidad: 'inteligente', caracteristica: 'cara alargada' } },
+  { id: 23, name: 'Ethan', image: 'images/23.png', traits: { estatura: 'de estatura media', pelo: 'negro', ojos: 'marrones', personalidad: 'inteligente', caracteristica: 'cara cuadrada' } },
+];
