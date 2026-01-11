945232f2b627d5a636d702c29d7fceb6d9501c4e
youssefdh840 <tkyoyoussef@gmail.com>
2026-01-11 23:34:55 +0100

    Add files via upload

---
+++ constants.tsx
@@ -0,0 +1,27 @@
+import { Character } from './types';
+
+export const CHARACTERS: Character[] = [
+  { id: 1, name: 'Andreas', image: 'images/1.png', traits: ['hombre', 'pelo negro', 'barba', 'serio', 'mirada fija', 'adulto'] },
+  { id: 2, name: 'Phoebe Buffay', image: 'images/2.png', traits: ['mujer', 'rubia', 'pelo largo', 'sonriente', 'pendientes', 'ojos claros'] },
+  { id: 3, name: 'Rachel', image: 'images/3.png', traits: ['mujer', 'pelo castaño', 'pelo liso', 'camiseta blanca', 'joven', 'cara cuadrada'] },
+  { id: 4, name: 'Victoria', image: 'images/4.png', traits: ['mujer', 'pelo largo', 'pelo castaño', 'mirada intensa', 'labios rosados', 'sin sonrisa'] },
+  { id: 5, name: 'Louati', image: 'images/5.png', traits: ['mujer', 'morena', 'pelo negro', 'pelo muy largo', 'labios oscuros', 'seria'] },
+  { id: 6, name: 'Ed Westwick', image: 'images/6.png', traits: ['hombre', 'pelo negro', 'traje', 'mirada seductora', 'moreno', 'cara cuadrada'] },
+  { id: 7, name: 'John Bradley', image: 'images/7.png', traits: ['hombre', 'pelo castaño', 'barba', 'cara redonda', 'mirada amable', 'pelo corto'] },
+  { id: 8, name: 'Victoria Ria', image: 'images/8.png', traits: ['mujer', 'pelo castaño', 'gafas de sol', 'en un barco', 'sonriendo', 'pelo largo'] },
+  { id: 9, name: 'Chishiya', image: 'images/9.png', traits: ['hombre', 'pelo rubio claro', 'pelo largo', 'chaqueta blanca', 'mirada astuta', 'humano', 'pelo liso'] },
+  { id: 10, name: 'Marilyn', image: 'images/10.png', traits: ['mujer', 'rubia', 'pelo corto', 'labios rojos', 'lunar', 'glamurosa'] },
+  { id: 11, name: 'Andreas Pietschma', image: 'images/11.png', traits: ['hombre', 'pelo corto', 'barba canosa', 'mirada triste', 'arrugas', 'adulto'] },
+  { id: 12, name: 'Louis Hofmann', image: 'images/12.png', traits: ['hombre', 'pelo rubio', 'joven', 'cara delgada', 'serio', 'ojos claros'] },
+  { id: 13, name: 'Killua Zoldyck', image: 'images/13.png', traits: ['hombre', 'pelo blanco', 'pelo pincho', 'ojos azules', 'ropa azul', 'niño', 'serio'] },
+  { id: 14, name: 'Sadie Sink', image: 'images/14.png', traits: ['mujer', 'pelirroja', 'pelo largo', 'ojos claros', 'pecas', 'joven'] },
+  { id: 15, name: 'Jo Byung-gyu', image: 'images/15.png', traits: ['hombre', 'pelo negro', 'pelo rizado', 'enfadado', 'mirada desafiante', 'joven'] },
+  { id: 16, name: 'Eiza González', image: 'images/16.png', traits: ['mujer', 'pelo castaño', 'labios pintados', 'pendientes grandes', 'pelo hacia atrás'] },
+  { id: 17, name: 'Eleven', image: 'images/17.png', traits: ['mujer', 'pelo muy corto', 'pelo castaño', 'mirada penetrante', 'joven', 'seria'] },
+  { id: 18, name: 'Joey Tribbiani', image: 'images/18.png', traits: ['hombre', 'pelo negro', 'frente ancha', 'sonrisa leve', 'atractivo', 'pelo corto'] },
+  { id: 19, name: 'Leonor', image: 'images/19.png', traits: ['mujer', 'pelo rubio', 'ojos claros', 'aspecto real', 'sonrisa formal', 'pelo largo'] },
+  { id: 20, name: 'Batistuta', image: 'images/20.png', traits: ['hombre', 'pelo largo', 'barba', 'pelo castaño', 'atleta', 'mirada ruda'] },
+  { id: 21, name: 'Emily', image: 'images/21.png', traits: ['mujer', 'pelo pelirroja', 'chaqueta verde', 'ojos oscuros', 'joven', 'pelo largo', 'mirada dulce'] },
+  { id: 22, name: 'Gojo', image: 'images/22.png', traits: ['hombre', 'pelo blanco', 'ojos azules', 'sonrisa confiada', 'pelo pincho', 'mirada brillante'] },
+  { id: 23, name: 'Aneirin Barnard', image: 'images/23.png', traits: ['hombre', 'pelo negro', 'pelo muy rizado', 'ojos oscuros', 'serio', 'cara alargada'] }
+];
